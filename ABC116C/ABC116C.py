d5bed0c733a1e4e8999f3141ee0c7b5cfa8732b8
Naoya <74296872+ce31062@users.noreply.github.com>
2022-08-07 21:19:48 +0900

    配列(enumurate)
    けんちょん鹿本の練習問題に掲載されている良問

---
+++ ABC116C/ABC116C.py
@@ -0,0 +1,23 @@
+#座標圧縮の問題
+N, M = map(int, input().split())
+D = [[] for i in range(N)]
+
+for i in range(M):
+    p, y = map(int, input().split())
+    D[p-1].append((y, i))
+
+ans = [None]*M
+# print(D) #[[(32, 0), (12, 2)], [(63, 1)]]
+
+for i, d in enumerate(D):
+    # print(i, d) #0 [(32, 0), (12, 2)], 1 [(63, 1)]
+    # print(d) #[(32, 0), (12, 2)], [(63, 1)]
+    d.sort()
+    # print(d) #[(12, 2), (32, 0)], [(63, 1)]
+    for k, (y,j) in enumerate(d):
+        ans[j] = str(i+1).zfill(6) + str(k+1).zfill(6)
+print(*ans, sep = "\n")
+
+#print(*ans)とすると，リストの要素がスペース区切りで出力される
+#printのオプションsepで，文字の区切りをスペース出なく，改行に指定
+#参考URL: https://qiita.com/ryupy/items/38b16a79b1d3f74b959f
